51e04fa080693f164d45fb88b63fc3e26df711ed
Ovidiu Anton Anton OVIDIUs <ovidiugabriel123456789@outlook.com>
2022-07-02 12:36:56 +0300

    Add project files.

---
+++ PrimulProiect_C++/PrimulProiect_C++.cpp
@@ -0,0 +1,69 @@
+﻿
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	double numar1, numar2, numar3;
+//	
+//	int medie = 0;
+//
+//	cout << "Introduce-ti numere de la tastatatura ! " << endl;
+//
+//	cin >> numar1 >> numar2 >> numar3;
+//
+//
+//	double medieAritmetica = (numar1 + numar2 + numar3 ) / 3;
+//
+//	cout << "Rezultat-ul mediei aritmetice este : " << medieAritmetica;
+//}
+
+
+//int main()
+//{
+//  /*  În preajma Crăciunului toţi suntem sau redevenim copii.
+//        Cerinţa
+//
+//        Scrie un program care afişează pe ecran o urare pentru cei dragi ţie!
+//        Date de intrare
+//
+//        Programul nu citește nicio valoare.
+//        Date de ieşire
+//
+//        Programul va afișa pe ecran o urare pentru cei dragi.
+//        Restricţii şi precizări
+//
+//        orice urare pentru cei dragi vouă va fi considerată corectă!!*/
+//
+//
+//    string urareCraciun = "Craciun fericit ! ";
+//
+//    cout << urareCraciun << endl;
+//
+//}
+
+
+//Cerința
+//
+//Se dau două numere naturale.Calculați diferenţa lor.
+//Restricții și precizări
+//
+//cele două numere sunt mai mici decât 2.000.000.000
+//se va scădea al doilea număr din primul
+//
+//
+//Exemplu
+//
+//Intrare
+//
+//5 3
+//
+//Ieșire
+//
+//2
+
+
+int main()
+{
+
+}
